d6eddd7d2390ec3ee42489e04de63f6a27293e9c
Anne-Sophie Kieslinger <anne.kieslinger@gmail.com>
2024-01-31 13:48:42 +0100

    Code and info on how to transform the Wang template
    - from neuropythy's .mgz file
    - to .gifti label file

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Population connective field modeling reveals retinotopic visual cortex organizat
 
 - uses dHCP data
 - more info see https://docs.google.com/document/d/1pcI7m-MnbXyYh-rkaZqW_hl4WtpSIwAdHC8ZdyhtiGs/edit?usp=sharing
+# 1. Getting the anatomical visual areas template (Wang 2015)
+
+`transform_wang_to_gifti.py`: Creates a gifti label file that is usable in the connectome workbench universe and readable in freeview as annotation. Needs a copy of neuropythy source code. Adjust input and output paths. 
--- transform_wang_to_gifti.py
+++ transform_wang_to_gifti.py
@@ -1,23 +1,69 @@
-"""Transform the Wang 2015 retinotopy template as published by neuropythy into a usable gifti file.
+"""Transform the Wang 2015 retinotopy template as published by neuropythy into a usable gifti label file.
 The template file has the extension '.mgz', which is a freesurfer format for volume files. However, we know
 that the template is on the surface, not in volumetric space. The shape of the data array when read 
 in with nibabel is (1, 1, 163842), 
 so the third dimension are the fsaverage 164k nodes. The array is transformed to 1 dimension and saved as 
-a gifti '.gii' image.
+a gifti '.gii' image. Then, a labeltable with the correct region labels is added. 
+Needs a copy of neuropythy source code.
 """
+import subprocess
+from pathlib import Path
+
 import nibabel as nib
 import numpy as np
 
-##----------------------CHANGE PATHS--------------------------------
-input_wangtemplate = "/data/u_kieslinger_software/code/neuropythy/neuropythy/lib/data/fsaverage/surf/rh.wang15_mplbl.v1_0.mgz"
-output_wangtemplate = "/data/p_02495/templates/template_visualtopography_wang2015/hemi-R_space-fsaverage_dens-164k_desc-visualtopographywang2015_label-maxprob_seg.gii"
-##--------------------------------------------------------------------------------
+labeltable = Path(__file__).parent.resolve() / "labeltable"
+print("Using labeltable: {labeltable}")
 
-img_wang_mgz = nib.load(input_wangtemplate)
-wang_data = img_wang_mgz.get_fdata()
-print(wang_data.shape)
+for hemi in ["l", "r"]:
+    ##----------------------CHANGE PATHS--------------------------------
+    input_wangtemplate = f"/data/u_kieslinger_software/code/neuropythy/neuropythy/lib/data/fsaverage/surf/{hemi}h.wang15_mplbl.v1_0.mgz"
+    output_wangtemplate = f"/data/p_02495/templates/template_visualtopography_wang2015/hemi-{hemi.upper()}_space-fsaverage_dens-164k_desc-visualtopographywang2015_label-maxprob_seg.label.gii"
+    ##--------------------------------------------------------------------------------
 
-img_wang = nib.gifti.GiftiImage(
-    darrays=[nib.gifti.GiftiDataArray(np.int32(np.squeeze(wang_data)))]
-)
-nib.save(img_wang, output_wangtemplate)
+    Path(output_wangtemplate).parent.mkdir(
+        exist_ok=True,
+        parents=True,
+    )
+    # save into intermediate file without label table
+    intermediate_metric_name = output_wangtemplate.split(".", maxsplit=1)
+    intermediate_metric_name = (
+        intermediate_metric_name[0] + "_metric" + intermediate_metric_name[1]
+    )
+
+    # import data from .mgz and save as gifti
+    img_wang_mgz = nib.load(input_wangtemplate)
+    wang_data = img_wang_mgz.get_fdata()
+    print(f"Original template data shape: {wang_data.shape}")
+
+    img_wang = nib.gifti.GiftiImage(
+        darrays=[
+            nib.gifti.GiftiDataArray(
+                np.int32(np.squeeze(wang_data)), intent="NIFTI_INTENT_LABEL"
+            )
+        ]
+    )
+    nib.save(img_wang, intermediate_metric_name)
+
+    # transform into label file with label table
+    proc = subprocess.run(
+        [
+            "wb_command",
+            "-metric-label-import",
+            intermediate_metric_name,
+            labeltable,
+            output_wangtemplate,
+        ],
+        check=False,
+        capture_output=True,
+        text=True,
+    )
+
+    assert (
+        proc.returncode == 0
+    ), f"Error while trying to convert metric file to label file using wb_command. Error: {proc.stderr}"
+
+    # remove intermediate file
+    subprocess.run(
+        ["rm", intermediate_metric_name], text=True, check=True, capture_output=True
+    )
