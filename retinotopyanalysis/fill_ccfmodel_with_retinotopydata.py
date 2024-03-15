ed6ff9a54eaa3acf7fe52ef97d0a9a55a9c3befc
Anne-Sophie Kieslinger <anne.kieslinger@gmail.com>
2024-03-15 13:26:10 +0100

    Change filenames of model output files
    - also rename variable to avoid name clash
    - allow models 'lc' or 'ccf'
    - print info about saved filename
    - change way to cast array to int (linter)

--- retinotopyanalysis/fill_ccfmodel_with_retinotopydata.py
+++ retinotopyanalysis/fill_ccfmodel_with_retinotopydata.py
@@ -3,7 +3,7 @@ from nilearn import surface
 import numpy as np
 
 
-def get_indices_roi(labels_area, visparc):
+def get_indices_roi(labels_area, visparc_array):
     """Get indices of vertices in gifti image that are located in a specific region of interest.
 
     Args:
@@ -14,43 +14,45 @@ def get_indices_roi(labels_area, visparc):
         numpy.array: n_vertices_roi. Indices of vertices that lie in the ROI.
     """
     indices_area = np.nonzero(
-        np.logical_or(visparc == labels_area[0], visparc == labels_area[1])
+        np.logical_or(visparc_array == labels_area[0], visparc_array == labels_area[1])
     )[0]
     return indices_area
 
 
+model = "lc"  # 'lc' or 'ccf'
 root_dir = "/data/p_02495/dhcp_derivatives"
-file_ccf_v0 = "/data/p_02495/dhcp_derivatives/ccfmodel/sub-{sub}/ses-{ses}/sub-{sub}_ses-{ses}_hemi-{hemi}_desc-test_v0i.gii"
+file_v0 = "/data/p_02495/dhcp_derivatives/ccfmodel/sub-{sub}/ses-{ses}/sub-{sub}_ses-{ses}_hemi-{hemi}_desc-{model}_v0i.gii"
 file_eccentricity = "/data/p_02495/dhcp_derivatives/dhcp_surface/sub-{sub}/ses-{ses}/space-T2w/anat/sub-{sub}_ses-{ses}_hemi-{hemi}_mesh-native_dens-native_desc-eccentretinotbenson2014_seg.shape.gii"
 file_angle = "/data/p_02495/dhcp_derivatives/dhcp_surface/sub-{sub}/ses-{ses}/space-T2w/anat/sub-{sub}_ses-{ses}_hemi-{hemi}_mesh-native_dens-native_desc-angleretinotbenson2014_seg.shape.gii"
 file_visparc = "{root_dir}/dhcp_surface/sub-{sub}/ses-{ses}/space-T2w/anat/sub-{sub}_ses-{ses}_hemi-{hemi}_mesh-native_dens-native_desc-visualtopographywang2015_label-maxprob_dparc.label.gii"
-output_ecc = "/data/p_02495/dhcp_derivatives/ccfmodel/sub-{sub}/ses-{ses}/sub-{sub}_ses-{ses}_hemi-{hemi}_desc-eccentricity_roi-v2_metric.gii"
-output_angle = "/data/p_02495/dhcp_derivatives/ccfmodel/sub-{sub}/ses-{ses}/sub-{sub}_ses-{ses}_hemi-{hemi}_desc-angle_roi-v2_metric.gii"
+output_ecc = "/data/p_02495/dhcp_derivatives/ccfmodel/sub-{sub}/ses-{ses}/sub-{sub}_ses-{ses}_hemi-{hemi}_label-eccentricity_desc-{model}_roi-v2_metric.gii"
+output_angle = "/data/p_02495/dhcp_derivatives/ccfmodel/sub-{sub}/ses-{ses}/sub-{sub}_ses-{ses}_hemi-{hemi}_label-angle_desc-{model}_roi-v2_metric.gii"
 
 sub = "CC00058XX09"
 ses = "11300"
 
+labels_v1 = [1, 2]  # see `ROIfiles_Labeling.txt` in this directory
+labels_v2 = [3, 4]
 for hemi in ["L"]:
-    labels_v1 = [1, 2]  # see `ROIfiles_Labeling.txt` in this directory
-    labels_v2 = [3, 4]
     visparc = surface.load_surf_data(
         file_visparc.format(root_dir=root_dir, sub=sub, ses=ses, hemi=hemi)
     )
     indices_v1 = get_indices_roi(labels_v1, visparc)
     indices_v2 = get_indices_roi(labels_v2, visparc)
-    ccf_v0 = np.int32(
-        surface.load_surf_data(file_ccf_v0.format(sub=sub, ses=ses, hemi=hemi))
-    )
+    ccf_v0 = surface.load_surf_data(
+        file_v0.format(sub=sub, ses=ses, hemi=hemi, model=model)
+    ).astype(int, copy=False)
+
     centers_v2 = ccf_v0[indices_v2]
 
     in_out = [
         (
             file_eccentricity.format(sub=sub, ses=ses, hemi=hemi),
-            output_ecc.format(sub=sub, ses=ses, hemi=hemi),
+            output_ecc.format(sub=sub, ses=ses, hemi=hemi, model=model),
         ),
         (
             file_angle.format(sub=sub, ses=ses, hemi=hemi),
-            output_angle.format(sub=sub, ses=ses, hemi=hemi),
+            output_angle.format(sub=sub, ses=ses, hemi=hemi, model=model),
         ),
     ]
 
@@ -67,3 +69,4 @@ for hemi in ["L"]:
             darrays=[nib.gifti.GiftiDataArray(np.float32(np.squeeze(ret_wholeb)))]
         )
         nib.save(img_gifti, retinotopy_out)
+        print(f"Saving {retinotopy_out}...")
