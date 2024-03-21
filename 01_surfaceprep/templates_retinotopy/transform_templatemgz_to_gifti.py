294dfc1a5f729875a1c3b97047a2f7532c7b7a91
Anne-Sophie Kieslinger <anne.kieslinger@gmail.com>
2024-03-21 16:11:44 +0100

    Transform benson2014 visual area files
    - adjust script structure to make more sense with new templ
    - print info about created/skipped templates

--- 01_surfaceprep/templates_retinotopy/transform_templatemgz_to_gifti.py
+++ 01_surfaceprep/templates_retinotopy/transform_templatemgz_to_gifti.py
@@ -15,13 +15,16 @@ from pathlib import Path
 import nibabel as nib
 import numpy as np
 
-labeltable = Path(__file__).parent.resolve() / "labeltable"
-print(f"Using labeltable: {labeltable}")
+parentdir = Path(__file__).parent.resolve()
+labeltable_wang = parentdir / "labeltable_wang2015"
+labeltable_benson = parentdir / "labeltable_benson2014"
+print(f"Using labeltables: {labeltable_wang} and {labeltable_benson}.")
 templates = [
     (
         "/data/u_kieslinger_software/code/neuropythy/neuropythy/lib/data/fsaverage/surf/{hemi}h.wang15_mplbl.v1_0.mgz",
         "hemi-{hemi}_space-fsaverage_dens-164k_desc-visualtopographywang2015_label-maxprob_seg.label.gii",
         np.int32,
+        labeltable_wang,
     ),
     (
         "/data/u_kieslinger_software/code/neuropythy/neuropythy/lib/data/fsaverage/surf/{hemi}h.benson14_eccen.v4_0.mgz",
@@ -33,17 +36,25 @@ templates = [
         "hemi-{hemi}_space-fsaverage_dens-164k_desc-angleretinotbenson2014_seg.shape.gii",
         np.float32,
     ),
+    (
+        "/data/u_kieslinger_software/code/neuropythy/neuropythy/lib/data/fsaverage/surf/{hemi}h.benson14_varea.v4_0.mgz",
+        "hemi-{hemi}_space-fsaverage_dens-164k_desc-retinotbenson2014_label-visarea_seg.label.gii",
+        np.int32,
+        labeltable_benson,
+    ),
 ]
 
 for template_paths in templates:
     for hemi in ["l", "r"]:
-        ##----------------------CHANGE PATHS--------------------------------
+
+        # fill paths for this hemi
         input_template = template_paths[0].format(hemi=hemi)
         output_template = Path(__file__).parent.resolve() / template_paths[1].format(
             hemi=hemi.upper()
         )
+
+        # we need template datatype to be able to cast the array to fit gifti standard
         dtype_templ = template_paths[2]
-        ##--------------------------------------------------------------------------------
 
         Path(output_template).parent.mkdir(
             exist_ok=True,
@@ -51,17 +62,6 @@ for template_paths in templates:
         )
 
         if not output_template.exists():
-
-            if dtype_templ == np.int32:
-                # save into intermediate file without label table
-                intermediate_metric_name = str(output_template).split(".", maxsplit=1)
-                intermediate_metric_name = (
-                    intermediate_metric_name[0]
-                    + "_metric"
-                    + intermediate_metric_name[1]
-                )
-            else:
-                intermediate_metric_name = str(output_template)
 
             # import data from .mgz and save as gifti
             img_mgz = nib.load(input_template)
@@ -71,16 +71,32 @@ for template_paths in templates:
             img_gifti = nib.gifti.GiftiImage(
                 darrays=[nib.gifti.GiftiDataArray(dtype_templ(np.squeeze(templ_data)))]
             )
-            nib.save(img_gifti, intermediate_metric_name)
 
-            if dtype_templ == np.int32:
+            if dtype_templ != np.int32:
+                nib.save(img_gifti, str(output_template))
+            else:
+                # save into intermediate file without label table
+                # needs conversion into proper 'label' file with label table later
+                intermediate_metric_name = str(output_template).split(".", maxsplit=1)
+                intermediate_metric_name = (
+                    intermediate_metric_name[0]
+                    + "_metric"
+                    + intermediate_metric_name[1]
+                )
+
+                nib.save(img_gifti, intermediate_metric_name)
+
+                assert (
+                    len(template_paths) == 4
+                ), "ERROR: Missing labeltable (must be 4. entry)"
+
                 # transform into label file with label table
                 proc = subprocess.run(
                     [
                         "wb_command",
                         "-metric-label-import",
                         intermediate_metric_name,
-                        labeltable,
+                        template_paths[3],
                         output_template,
                     ],
                     check=False,
@@ -93,9 +109,9 @@ for template_paths in templates:
                 ), f"Error while trying to convert metric file to label file using wb_command. Error: {proc.stderr}"
 
                 # remove intermediate file
-                subprocess.run(
-                    ["rm", intermediate_metric_name],
-                    text=True,
-                    check=True,
-                    capture_output=True,
-                )
+                Path(intermediate_metric_name).unlink()
+            print(f"Saving template {input_template} at {output_template}...")
+        else:
+            print(
+                f"Template {input_template} already exists at {output_template}. Skipping this..."
+            )
