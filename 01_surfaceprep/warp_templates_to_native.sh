7d295ead0f23549a8aa7e2bc0d6735adbb8cd7a2
Anne-Sophie Kieslinger <anne.kieslinger@gmail.com>
2024-03-21 17:15:20 +0100

    Make file paths clearer and check if file exists before creating

--- 01_surfaceprep/warp_templates_to_native.sh
+++ 01_surfaceprep/warp_templates_to_native.sh
@@ -13,6 +13,10 @@ path_HCPtemplates_standardmeshatlases=$5
 path_surfacetemplate=$6
 path_fsaverage=$7
 
+# prepare output directory for subject in T2w space
+path_out_native=$path_output_data/sub-$sub/ses-$ses/space-T2w/anat
+mkdir -p $path_out_native
+
 for hemi in left right; do
     if [ $hemi = "left" ]; then
         hemi_upper="L"
@@ -20,72 +24,98 @@ for hemi in left right; do
         hemi_upper="R"
     fi
 
-    visareas_wang_template=$path_script/templates_retinotopy/hemi-${hemi_upper}_space-fsaverage_dens-164k_desc-visualtopographywang2015_label-maxprob_seg.label.gii
-    visareas_benson_template=$path_script/templates_retinotopy/hemi-${hemi_upper}_space-fsaverage_dens-164k_desc-retinotbenson2014_label-visarea_seg.label.gii
-    ecc_template=$path_script/templates_retinotopy/hemi-${hemi_upper}_space-fsaverage_dens-164k_desc-eccentretinotbenson2014_seg.shape.gii
-    angl_template=$path_script/templates_retinotopy/hemi-${hemi_upper}_space-fsaverage_dens-164k_desc-angleretinotbenson2014_seg.shape.gii
+    # registrations necessary to make the transformation from fsaverage to native
+    # will be created if they don't exist already
+    registration_from_dhcpSym_to_fsaverage="$path_script/standard_registrations/hemi-${hemi_upper}_from-dhcpSym_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii"
+    registration_from_native_to_fsaverage="$path_output_data/sub-$sub/ses-$ses/space-dhcpSym/surface_transforms/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_from-native_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii"
+
+    # output of previous step, see alignment folder
+    # check if this exists, terminate otherwise
+    registration_from_native_to_dhcpSym="$path_output_data/sub-$sub/ses-$ses/space-dhcpSym/surface_transforms/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_from-native_to-dhcpSym40_dens-32k_mode-sphere.reg40.surf.gii"
+    if [ ! -f $registration_from_native_to_dhcpSym ]; then
+        echo "Registration from native to dhcpSym is missing. Please execute alignment folder main script first."
+        echo "Terminating..."
+        exit 1
+    fi
+
+    # templates in fsaverage space
+    # come with the repo, for more info see templates_retinotopy README
+    path_templates_fsaverage="$path_script/templates_retinotopy/hemi-${hemi_upper}_space-fsaverage_dens-164k"
+    visareas_wang_template="${path_templates_fsaverage}_desc-visualtopographywang2015_label-maxprob_seg.label.gii"
+    visareas_benson_template="${path_templates_fsaverage}_desc-retinotbenson2014_label-visarea_seg.label.gii"
+    ecc_template="${path_templates_fsaverage}_desc-eccentretinotbenson2014_seg.shape.gii"
+    angl_template="${path_templates_fsaverage}_desc-angleretinotbenson2014_seg.shape.gii"
+
+    # output
+    path_out_native_hemi="$path_out_native/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_mesh-native_dens-native"
+    visareas_wang_native="${path_out_native_hemi}_desc-visualtopographywang2015_label-maxprob_dparc.label.gii"
+    visareas_benson_native="${path_out_native_hemi}_desc-retinotbenson2014_label-visarea_dparc.label.gii"
+    ecc_native="${path_out_native_hemi}_desc-eccentretinotbenson2014_seg.shape.gii"
+    pangle_native="${path_out_native_hemi}_desc-angleretinotbenson2014_seg.shape.gii"
 
     # 1. Combine registration from dhcpSym to fs_lr to fsaverage
-    registration_from_dhcpSym_to_fsaverage=$path_script/registrations/hemi-${hemi_upper}_from-dhcpSym_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii
+    # TODO check if 164k fsaverage should be used here???
     if [ ! -f $registration_from_dhcpSym_to_fsaverage ]; then
         wb_command -surface-sphere-project-unproject \
-            $path_script/registrations/dHCP_HCP-YA.MSMStrain.sphere.reg.surf.gii `# sphere-in: sphere with desired output mesh: dhcpsym registered to fslr` \
+            $path_script/standard_registrations/dHCP_HCP-YA.MSMStrain.sphere.reg.surf.gii `# sphere-in: sphere with desired output mesh: dhcpsym registered to fslr` \
             $path_HCPtemplates_standardmeshatlases/${hemi_upper}.sphere.32k_fs_LR.surf.gii `# sphere-project-to: sphere that aligns with sphere-in: fs_lr` \
             $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fs_LR-deformed_to-fsaverage.${hemi_upper}.sphere.32k_fs_LR.surf.gii `# sphere-unproject-from: sphere-project-to deformed to desired output space: fs_lr registered to fsaverage` \
             $registration_from_dhcpSym_to_fsaverage`# sphere-out: output sphere`
     fi
 
     # 2. Combine registration from native to dhcpSym to fsaverage
-    registration_from_native_to_fsaverage=$path_output_data/sub-$sub/ses-$ses/space-dhcpSym/surface_transforms/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_from-native_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii
     if [ ! -f $registration_from_native_to_fsaverage ]; then
         wb_command -surface-sphere-project-unproject \
-            $path_output_data/sub-$sub/ses-$ses/space-dhcpSym/surface_transforms/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_from-native_to-dhcpSym40_dens-32k_mode-sphere.reg40.surf.gii `# sphere-in: sphere with desired output mesh: native registered to dhcpSym` \
+            $registration_from_native_to_dhcpSym `# sphere-in: sphere with desired output mesh: native registered to dhcpSym` \
             $path_surfacetemplate/week-40_hemi-${hemi}_space-dhcpSym_dens-32k_sphere.surf.gii `# sphere-project-to: sphere that aligns with sphere-in: dhcpSym` \
-            $path_script/registrations/hemi-${hemi_upper}_from-dhcpSym_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii`# sphere-unproject-from: sphere-project-to deformed to desired output space: dhcpSym registered to fsaverage` \
+            $registration_from_dhcpSym_to_fsaverage`# sphere-unproject-from: sphere-project-to deformed to desired output space: dhcpSym registered to fsaverage` \
             $registration_from_native_to_fsaverage `# sphere-out: output sphere`
     fi
 
     # needed here as output: native sphere registered to fsaverage
     # 4. Resample retinotopy templates onto native
-    mkdir -p $path_output_data/sub-$sub/ses-$ses/space-T2w/anat/
-    wb_command -label-resample \
-        $visareas_wang_template `# label-in: visareas_wang_template` \
-        $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fsaverage_std_sphere.${hemi_upper}.164k_fsavg_${hemi_upper}.surf.gii `# current sphere: fsavg 164k ` \
-        $path_output_data/sub-$sub/ses-$ses/space-dhcpSym/surface_transforms/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_from-native_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii `# new sphere: sphere in register with current sphere and desired mesh` \
-        ADAP_BARY_AREA \
-        $path_output_data/sub-$sub/ses-$ses/space-T2w/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_mesh-native_dens-native_desc-visualtopographywang2015_label-maxprob_dparc.label.gii \
-        -area-surfs \
-        $path_fsaverage/surf/${hemi:0:1}h_midthickness_surf.gii \
-        $path_bids_data/sub-$sub/ses-$ses/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_space-T2w_midthickness.surf.gii
-
-    wb_command -label-resample \
-        $visareas_benson_template `# label-in: visareas_wang_template` \
-        $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fsaverage_std_sphere.${hemi_upper}.164k_fsavg_${hemi_upper}.surf.gii `# current sphere: fsavg 164k ` \
-        $path_output_data/sub-$sub/ses-$ses/space-dhcpSym/surface_transforms/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_from-native_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii `# new sphere: sphere in register with current sphere and desired mesh` \
-        ADAP_BARY_AREA \
-        $path_output_data/sub-$sub/ses-$ses/space-T2w/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_mesh-native_dens-native_desc-retinotbenson2014_label-visarea_dparc.label.gii \
-        -area-surfs \
-        $path_fsaverage/surf/${hemi:0:1}h_midthickness_surf.gii \
-        $path_bids_data/sub-$sub/ses-$ses/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_space-T2w_midthickness.surf.gii
-
-    wb_command -metric-resample \
-        $ecc_template \
-        $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fsaverage_std_sphere.${hemi_upper}.164k_fsavg_${hemi_upper}.surf.gii `# current sphere: fsavg 164k ` \
-        $path_output_data/sub-$sub/ses-$ses/space-dhcpSym/surface_transforms/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_from-native_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii `# new sphere: sphere in register with current sphere and desired mesh` \
-        ADAP_BARY_AREA \
-        $path_output_data/sub-$sub/ses-$ses/space-T2w/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_mesh-native_dens-native_desc-eccentretinotbenson2014_seg.shape.gii \
-        -area-surfs \
-        $path_fsaverage/surf/${hemi:0:1}h_midthickness_surf.gii \
-        $path_bids_data/sub-$sub/ses-$ses/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_space-T2w_midthickness.surf.gii
-
-    wb_command -metric-resample \
-        $angl_template \
-        $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fsaverage_std_sphere.${hemi_upper}.164k_fsavg_${hemi_upper}.surf.gii `# current sphere: fsavg 164k ` \
-        $path_output_data/sub-$sub/ses-$ses/space-dhcpSym/surface_transforms/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_from-native_to-fsaverage32k_dens-32k_mode-sphere_reg.surf.gii `# new sphere: sphere in register with current sphere and desired mesh` \
-        ADAP_BARY_AREA \
-        $path_output_data/sub-$sub/ses-$ses/space-T2w/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_mesh-native_dens-native_desc-angleretinotbenson2014_seg.shape.gii \
-        -area-surfs \
-        $path_fsaverage/surf/${hemi:0:1}h_midthickness_surf.gii \
-        $path_bids_data/sub-$sub/ses-$ses/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_space-T2w_midthickness.surf.gii
+    if [ ! -f $visareas_wang_native ]; then
+        wb_command -label-resample \
+            $visareas_wang_template `# label-in: visareas_wang_template` \
+            $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fsaverage_std_sphere.${hemi_upper}.164k_fsavg_${hemi_upper}.surf.gii `# current sphere: fsavg 164k ` \
+            $registration_from_native_to_fsaverage \
+            ADAP_BARY_AREA \
+            $visareas_wang_native \
+            -area-surfs \
+            $path_fsaverage/surf/${hemi:0:1}h_midthickness_surf.gii \
+            $path_bids_data/sub-$sub/ses-$ses/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_space-T2w_midthickness.surf.gii
+    fi
+    if [ ! -f $visareas_benson_native ]; then
+        wb_command -label-resample \
+            $visareas_benson_template `# label-in: visareas_wang_template` \
+            $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fsaverage_std_sphere.${hemi_upper}.164k_fsavg_${hemi_upper}.surf.gii `# current sphere: fsavg 164k ` \
+            $registration_from_native_to_fsaverage \
+            ADAP_BARY_AREA \
+            $visareas_benson_native \
+            -area-surfs \
+            $path_fsaverage/surf/${hemi:0:1}h_midthickness_surf.gii \
+            $path_bids_data/sub-$sub/ses-$ses/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_space-T2w_midthickness.surf.gii
+    fi
+    if [ ! -f $ecc_native ]; then
+        wb_command -metric-resample \
+            $ecc_template \
+            $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fsaverage_std_sphere.${hemi_upper}.164k_fsavg_${hemi_upper}.surf.gii `# current sphere: fsavg 164k ` \
+            $registration_from_native_to_fsaverage \
+            ADAP_BARY_AREA \
+            $ecc_native \
+            -area-surfs \
+            $path_fsaverage/surf/${hemi:0:1}h_midthickness_surf.gii \
+            $path_bids_data/sub-$sub/ses-$ses/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_space-T2w_midthickness.surf.gii
+    fi
+    if [ ! -f $pangle_native ]; then
+        wb_command -metric-resample \
+            $angl_template \
+            $path_HCPtemplates_standardmeshatlases/resample_fsaverage/fsaverage_std_sphere.${hemi_upper}.164k_fsavg_${hemi_upper}.surf.gii `# current sphere: fsavg 164k ` \
+            $registration_from_native_to_fsaverage \
+            ADAP_BARY_AREA \
+            $pangle_native -area-surfs \
+            $path_fsaverage/surf/${hemi:0:1}h_midthickness_surf.gii \
+            $path_bids_data/sub-$sub/ses-$ses/anat/sub-${sub}_ses-${ses}_hemi-${hemi_upper}_space-T2w_midthickness.surf.gii
+    fi
 
 done
