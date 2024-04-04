6ca4f6846fd4806592f8f734231720eeafff8a81
Anne-Sophie Kieslinger <anne.kieslinger@gmail.com>
2024-04-04 13:30:04 +0200

    Restrict to vertices with correlation threshold

--- 03_retinotopyanalysis/fill_ccfmodel_with_retinotopydata.py
+++ 03_retinotopyanalysis/fill_ccfmodel_with_retinotopydata.py
@@ -1,3 +1,7 @@
+"""
+Project V1 retinotopy template values to V2 based on cortico-connective field results.
+"""
+
 from argparse import ArgumentParser
 
 import nibabel as nib
@@ -13,8 +17,13 @@ PREFIX_SUB_TEMPLATE = (
 )
 
 PATH_v0 = "{prefix_model}_desc-{model}_v0i.gii"
-OUTPUT_ECC = "{prefix_model}_label-eccentricity_desc-{model}_roi-v2_metric.gii"
-OUTPUT_PANGLE = "{prefix_model}_label-polarangle_desc-{model}_roi-v2_metric.gii"
+PATH_r = "{prefix_model}_desc-{model}_r.gii"
+OUTPUT_ECC = (
+    "{prefix_model}_label-eccentricity_desc-{model}_roi-v2th{threshold}_metric.gii"
+)
+OUTPUT_PANGLE = (
+    "{prefix_model}_label-polarangle_desc-{model}_roi-v2th{threshold}_metric.gii"
+)
 
 PATH_ECCENTRICITY = "{prefix_sub_template}_desc-eccentretinotbenson2014_seg.shape.gii"
 PATH_ANGLE = "{prefix_sub_template}_desc-angleretinotbenson2014_seg.shape.gii"
@@ -45,6 +54,16 @@ def parse_args():
         required=True,
         help="Session being processed",
     )
+    parser.add_argument(
+        "-th",
+        "--threshold",
+        required=False,
+        default=0.1,
+        help=(
+            "Correlation coefficient (r) threshold from model results. "
+            "Vertices that pass the threshold will be filled with retinotopy data."
+        ),
+    )
 
     args = parser.parse_args()
     return args
@@ -69,7 +88,6 @@ def get_indices_roi(labels_area, visparc_array):
 def main():
     args = parse_args()
     # -d /data/p_02495/dhcp_derivatives -sub CC00065XX08 -ses 18600
-    # TODO restrict to voxels r>0.1 here
 
     for model in ["real", "simulated"]:
 
@@ -93,11 +111,19 @@ def main():
             in_out = [
                 (
                     PATH_ECCENTRICITY.format(prefix_sub_template=prefix_sub_template),
-                    OUTPUT_ECC.format(prefix_model=prefix_model, model=model),
+                    OUTPUT_ECC.format(
+                        prefix_model=prefix_model,
+                        model=model,
+                        threshold=str(args.threshold).replace(".", ""),
+                    ),
                 ),
                 (
                     PATH_ANGLE.format(prefix_sub_template=prefix_sub_template),
-                    OUTPUT_PANGLE.format(prefix_model=prefix_model, model=model),
+                    OUTPUT_PANGLE.format(
+                        prefix_model=prefix_model,
+                        model=model,
+                        threshold=str(args.threshold).replace(".", ""),
+                    ),
                 ),
             ]
 
@@ -110,6 +136,10 @@ def main():
                 ccf_v0 = surface.load_surf_data(
                     PATH_v0.format(prefix_model=prefix_model, model=model)
                 ).astype(int, copy=False)
+
+                ccf_r = surface.load_surf_data(
+                    PATH_r.format(prefix_model=prefix_model, model=model)
+                )
             except ValueError:
                 print(
                     f"No model results found under {PATH_v0.format(prefix_model=prefix_model, model=model)}."
@@ -120,9 +150,15 @@ def main():
             indices_v1 = get_indices_roi(LABELS_V1, visparc)
             indices_v2 = get_indices_roi(LABELS_V2, visparc)
 
+            # fill only those vertices that pass the correlation threshold
+            indices_v2_success = np.intersect1d(
+                indices_v2,
+                np.nonzero(np.abs(ccf_r) > float(args.threshold)),
+                assume_unique=True,
+            )
             # get indices into V1 for each area V2 vertex
             # indices refer to V1 vertex index inside V1, not whole brain!
-            centers_v2 = ccf_v0[indices_v2]
+            centers_v2 = ccf_v0[indices_v2_success]
 
             # indices to voxels in v1 array that are the centers for each v2 voxel
             # indexv1_centers_v2 = indices_v1[centers_v2]
@@ -130,7 +166,7 @@ def main():
 
                 # load template data in sub space
                 ret = surface.load_surf_data(retinotopy_in)
-
+                # TODO check if these really are the indices into just  v1
                 # restrict to V1
                 ret_v1 = ret[indices_v1]
 
@@ -141,7 +177,7 @@ def main():
                 ret_wholeb = np.zeros(ret.shape)
 
                 # fill retinotopy values into v2
-                ret_wholeb[indices_v2] = ret_v2
+                ret_wholeb[indices_v2_success] = ret_v2
 
                 # save as gifti
                 img_gifti = nib.gifti.GiftiImage(
