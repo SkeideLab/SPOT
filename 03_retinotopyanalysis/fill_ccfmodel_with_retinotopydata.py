61387473d452f9729cc914dbdeeb3aed8e66aaac
Anne-Sophie Kieslinger <anne.kieslinger@gmail.com>
2024-04-04 15:14:47 +0200

    Combine filling retinotopy results and comparing models

--- 03_retinotopyanalysis/fill_ccfmodel_with_retinotopydata.py
+++ 03_retinotopyanalysis/fill_ccfmodel_with_retinotopydata.py
@@ -1,8 +1,12 @@
 """
-Project V1 retinotopy template values to V2 based on cortico-connective field results.
+Project V1 retinotopy template values to V2 based on cortico-connective field results. 
+Print the mean square differences between 
+ccf results from real vs simulated data and between 
+ccf results from real data vs benson template data.
 """
 
 from argparse import ArgumentParser
+from pathlib import Path
 
 import nibabel as nib
 import numpy as np
@@ -85,52 +89,67 @@ def get_indices_roi(labels_area, visparc_array):
     return indices_area
 
 
+def calc_msd(param_area1, param_area2):
+    # mean squared difference between two different model results in an area
+    n1 = param_area1.size
+    n2 = param_area2.size
+    assert (
+        n1 == n2
+    ), "areas don't have the same number of vertices and cannot be compared!"
+
+    return np.sum((param_area1 - param_area2) ** 2) / n1
+
+
+def print_differences(retinotopy_dict, hemi, indices_v2):
+    for model_1, model_2 in [("real", "benson"), ("real", "simulated")]:
+        for param in ["eccentricity", "polarangle"]:
+            msd = calc_msd(
+                retinotopy_dict[(hemi, model_1, param)][indices_v2],
+                retinotopy_dict[(hemi, model_2, param)][indices_v2],
+            )
+            print(f"MSD for {param}: comparison {model_1} vs {model_2}: {msd}")
+
+
 def main():
     args = parse_args()
     # -d /data/p_02495/dhcp_derivatives -sub CC00065XX08 -ses 18600
 
-    for model in ["real", "simulated"]:
+    retinotopy_dict = {}
+    in_out = {
+        "eccentricity": OUTPUT_ECC,
+        "polarangle": OUTPUT_PANGLE,
+    }
+    for hemi in ["L", "R"]:
+        # FORMAT PATHS FOR INPUT AND OUTPUT
+        prefix_model = PREFIX_MODEL.format(
+            root_dir=args.derivatives_directory,
+            sub=args.sub,
+            ses=args.ses,
+            hemi=hemi,
+        )
+        prefix_sub_template = PREFIX_SUB_TEMPLATE.format(
+            root_dir=args.derivatives_directory,
+            sub=args.sub,
+            ses=args.ses,
+            hemi=hemi,
+        )
 
-        for hemi in ["L", "R"]:
+        # LOAD DATA
+        visparc = surface.load_surf_data(
+            PATH_VISPARC.format(prefix_sub_template=prefix_sub_template)
+        )
+        indices_v1 = get_indices_roi(LABELS_V1, visparc)
+        indices_v2 = get_indices_roi(LABELS_V2, visparc)
 
-            # FORMAT PATHS FOR INPUT AND OUTPUT
-            prefix_model = PREFIX_MODEL.format(
-                root_dir=args.derivatives_directory,
-                sub=args.sub,
-                ses=args.ses,
-                hemi=hemi,
-            )
-            prefix_sub_template = PREFIX_SUB_TEMPLATE.format(
-                root_dir=args.derivatives_directory,
-                sub=args.sub,
-                ses=args.ses,
-                hemi=hemi,
-            )
+        retinotopy_dict[(hemi, "benson", "eccentricity")] = surface.load_surf_data(
+            PATH_ECCENTRICITY.format(prefix_sub_template=prefix_sub_template)
+        )
 
-            # list of corresponding input/output transformations
-            in_out = [
-                (
-                    PATH_ECCENTRICITY.format(prefix_sub_template=prefix_sub_template),
-                    OUTPUT_ECC.format(
-                        prefix_model=prefix_model,
-                        model=model,
-                        threshold=str(args.threshold).replace(".", ""),
-                    ),
-                ),
-                (
-                    PATH_ANGLE.format(prefix_sub_template=prefix_sub_template),
-                    OUTPUT_PANGLE.format(
-                        prefix_model=prefix_model,
-                        model=model,
-                        threshold=str(args.threshold).replace(".", ""),
-                    ),
-                ),
-            ]
+        retinotopy_dict[(hemi, "benson", "polarangle")] = surface.load_surf_data(
+            PATH_ANGLE.format(prefix_sub_template=prefix_sub_template)
+        )
 
-            # LOAD DATA
-            visparc = surface.load_surf_data(
-                PATH_VISPARC.format(prefix_sub_template=prefix_sub_template)
-            )
+        for model in ["real", "simulated"]:
 
             try:
                 ccf_v0 = surface.load_surf_data(
@@ -147,9 +166,6 @@ def main():
                 print(" Skipping this...", flush=True)
                 continue
 
-            indices_v1 = get_indices_roi(LABELS_V1, visparc)
-            indices_v2 = get_indices_roi(LABELS_V2, visparc)
-
             # fill only those vertices that pass the correlation threshold
             indices_v2_success = np.intersect1d(
                 indices_v2,
@@ -162,32 +178,53 @@ def main():
 
             # indices to voxels in v1 array that are the centers for each v2 voxel
             # indexv1_centers_v2 = indices_v1[centers_v2]
-            for retinotopy_in, retinotopy_out in in_out:
+            for param in ["eccentricity", "polarangle"]:
 
-                # load template data in sub space
-                ret = surface.load_surf_data(retinotopy_in)
+                retinotopy_out = in_out[param].format(
+                    prefix_model=prefix_model,
+                    model=model,
+                    threshold=str(args.threshold).replace(".", ""),
+                )
+
                 # TODO check if these really are the indices into just  v1
                 # restrict to V1
-                ret_v1 = ret[indices_v1]
+                ret_v1 = retinotopy_dict[(hemi, "benson", param)][indices_v1]
 
                 # project data from V1 vertices to V2
                 ret_v2 = ret_v1[centers_v2]
 
                 # make empty wholebrain
-                ret_wholeb = np.zeros(ret.shape)
+                ret_wholeb = np.zeros(retinotopy_dict[(hemi, "benson", param)].shape)
 
                 # fill retinotopy values into v2
                 ret_wholeb[indices_v2_success] = ret_v2
 
-                # save as gifti
-                img_gifti = nib.gifti.GiftiImage(
-                    darrays=[
-                        nib.gifti.GiftiDataArray(np.float32(np.squeeze(ret_wholeb)))
-                    ]
-                )
+                # keep data for model comparisons later
+                retinotopy_dict[(hemi, model, param)] = ret_wholeb
 
-                nib.save(img_gifti, retinotopy_out)
-                print(f"Saving {retinotopy_out}...")
+                if not Path(retinotopy_out).exists():
+
+                    # save as gifti
+                    img_gifti = nib.gifti.GiftiImage(
+                        darrays=[
+                            nib.gifti.GiftiDataArray(np.float32(np.squeeze(ret_wholeb)))
+                        ]
+                    )
+
+                    nib.save(img_gifti, retinotopy_out)
+                    print(f"Saving {retinotopy_out}...")
+
+                else:
+                    print(f"File {retinotopy_out} already exists. Skipping the save...")
+
+        # CALCULATE DIFFERENCES BETWEEN MODELS PER HEMI
+        # calculate mean square differences between models for eccentricity and angle
+        # TODO decide how to deal with the thresholded vertices
+        # might result in different numbers of 'active' vertices for real vs simulated data
+        # can these two datasets be compared then?
+        # 'inactive' vertices get filled with 0, this could bias the msd
+        # use intersection of active vertices instead?
+        print_differences(retinotopy_dict, hemi, indices_v2)
 
 
 if __name__ == "__main__":
