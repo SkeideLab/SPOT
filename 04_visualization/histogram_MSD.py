76033215fcb5f979431c0258a5ce5207f116c56c
Sohyeon9367 <bsh00156@gmail.com>
2024-04-23 15:07:03 +0200

    Histogram

---
+++ 04_visualization/histogram_MSD.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+msd_value = pd.read_csv("/data/p_02915/SPOT/MSD.csv")
+
+for hemi in ["L", "R"]:
+    for param in ["eccentricity", "polarangle"]:
+        Benson = msd_value.loc[:30,f"{hemi}_{param}_real_benson"]
+        Simulated = msd_value.loc[:30,f"{hemi}_{param}_real_simulated"]
+        differences = Benson - Simulated
+        num_bins = len(differences)
+
+        fig, ax = plt.subplots()
+
+        # the histogram of the data
+        n, bins, patches = ax.hist(differences, num_bins)
+
+        ax.set_xlabel('MSD differences', fontsize = 20)
+        ax.set_ylabel('Number of subject', fontsize = 20)
+        ax.set_title(f"Histogram of MSD_Benson - MSD_Simulated data on hemisphere {hemi} for {param}", fontsize = 20)
+        ax.tick_params(axis='x', labelsize=15)  # Adjust the fontsize as needed
+        ax.tick_params(axis='y', labelsize=15)  # Adjust the fontsize as needed
+   
+        plt.show()
---
+++ 04_visualization/histogram_median_r.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("/data/p_02915/SPOT/median_r.csv")
+
+for hemi in ["left", "right"]:
+        
+    datasource = "real"
+    median_correlation = np.median(df[f"{hemi}_{datasource}"])
+    print(f"Median correlation of {datasource} data on hemisphere {hemi} is {median_correlation}.")
+    num_bins = len(df[f"{hemi}_{datasource}"])
+
+    fig, ax = plt.subplots()
+
+    # the histogram of the data
+    n, bins, patches = ax.hist(df[f"{hemi}_{datasource}"], num_bins, density=True)
+
+    ax.set_xlabel('Median correlation')
+    ax.set_ylabel('Number of subject')
+    ax.set_title(f"Histogram of {datasource} data on hemisphere {hemi}: median correlation = {median_correlation}.")
+    
+    plt.show()
